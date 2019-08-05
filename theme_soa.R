e209e7ad2e3f53c21860091d9a50a11f70ce6033
Samuel Castillo <castillo.sam.d@gmail.com>
2019-08-05 17:06:41 -0500

    Added slides for Sam's section
    1.  Upgraded the graphics engine to Cairo (This makes anti-aliased graphs on windows.  On a mac it does by default).
    2.  Added data on hospital readmissions and NY health insurance premiums from .gov data sets.  
    3.  Added slides to back

---
+++ theme_soa.R
@@ -0,0 +1,71 @@
+#' Theme inspired by fivethirtyeight.com plots
+#'
+#' Theme inspired by the plots on
+#' \href{fivethirtyeight.com}{http://fivethirtyeight.com}.
+#'
+#' @inheritParams ggplot2::theme_grey
+#' @family themes fivethirtyeight
+#' @export
+#' @example inst/examples/ex-theme_fivethirtyeight.R
+#' @importFrom grid unit
+theme_soa <- function(base_size = 12, base_family = "sans") {
+  colors <- deframe(ggthemes::ggthemes_data[["fivethirtyeight"]])
+  (theme_foundation(base_size = base_size, base_family = base_family)
+    + theme(
+      line = element_line(colour = "black"),
+      rect = element_rect(fill = colors["Light Gray"],
+                          linetype = 0, colour = NA),
+      text = element_text(colour = colors["Dark Gray"]),
+      legend.background = element_rect(),
+      legend.position = "bottom",
+      legend.direction = "horizontal",
+      legend.box = "vertical",
+      panel.grid = element_line(colour = NULL),
+      panel.grid.major =
+        element_line(colour = colors["Medium Gray"]),
+      panel.grid.minor = element_blank(),
+      # unfortunately, can't mimic subtitles TODO!
+      plot.title = element_text(hjust = 0, size = rel(1.5), face = "bold"),
+      plot.margin = unit(c(1, 1, 1, 1), "lines"),
+      strip.background = element_rect()))
+}
+
+#' fivethirtyeight.com color palette
+#'
+#' The standard three-color fivethirtyeight.com palette for line plots comprises
+#' blue, red, and green.
+#'
+#' @family colour fivethirtyeight
+#' @export
+#' @example inst/examples/ex-fivethirtyeight_pal.R
+fivethirtyeight_pal <- function() {
+  colors <- deframe(ggthemes::ggthemes_data[["fivethirtyeight"]])
+  values <- unname(colors[c("Blue", "Red", "Green")])
+  max_n <- length(values)
+  f <- manual_pal(values)
+  attr(f, "max_n") <- max_n
+  f
+}
+
+#' fivethirtyeight.com color scales
+#'
+#' Color scales using the colors in the fivethirtyeight graphics.
+#'
+#' @inheritParams ggplot2::scale_colour_hue
+#' @family colour fivethirtyeight
+#' @rdname scale_fivethirtyeight
+#' @seealso \code{\link{theme_fivethirtyeight}()} for examples.
+#' @export
+scale_colour_fivethirtyeight <- function(...) {
+  discrete_scale("colour", "economist", fivethirtyeight_pal(), ...)
+}
+
+#' @rdname scale_fivethirtyeight
+#' @export
+scale_color_fivethirtyeight <- scale_colour_fivethirtyeight
+
+#' @rdname scale_fivethirtyeight
+#' @export
+scale_fill_fivethirtyeight <- function(...) {
+  discrete_scale("fill", "economist", fivethirtyeight_pal(), ...)
+}
